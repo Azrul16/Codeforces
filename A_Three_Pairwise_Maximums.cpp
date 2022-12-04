002f615d3923946a4985f629ecf1744705cb7f8a
Azrul Amaline <azrul.amaline16@gmail.com>
2022-12-04 21:46:10 +0600

    04/Dec/2022

---
+++ A_Three_Pairwise_Maximums.cpp
@@ -0,0 +1,37 @@
+//Aurthor:- Azrul Amaline
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl;
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define goBabygo {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int solve(){
+    ll n,m,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    cin>>a>>b>>c;
+    
+    
+}
+
+int main()
+{   goBabygo
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+}
