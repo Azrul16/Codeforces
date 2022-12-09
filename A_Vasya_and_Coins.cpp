6694037684c2b294016c7626ee02ca31aaa60766
Azrul Amaline <azrul.amaline16@gmail.com>
2022-12-09 10:56:00 +0600

    09/Dec/2022

---
+++ A_Vasya_and_Coins.cpp
@@ -0,0 +1,48 @@
+//Aurthor:- Azrul Amaline
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl;
+#define YES cout<<"YES";
+#define NO cout<<"NO";
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define goBabygo {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int solve(){
+    ll n,m,i,j,k,x,y,z,a,b,c,cnt=0,tmp=0,sum=0;
+    cin>>a>>b;
+    if(a>0&&b>0){
+        cout<<(a+(2*b))+1;
+    }
+    else if(a==0)
+    cout<<1;
+    else if(b==0)
+    cout<<a+1;
+
+    nl
+
+    
+}
+
+int main()
+{   goBabygo
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+}
