fd61f429b7c967085503040289e807edeee9a4c8
Azrul Amaline <azrul.amaline16@gmail.com>
2022-12-10 15:03:31 +0600

    10/Dec/2022

---
+++ A_And_Then_There_Were_K.cpp
@@ -0,0 +1,45 @@
+//Aurthor:- Azrul Amaline
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl;
+#define YES cout<<"YES";
+#define NO cout<<"NO";
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define goBabygo {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int solve(){
+    int n,m=1,i,j,k,x,y,z,a,b,c,cnt=0,tmp=0,sum=0;
+	cin>>n;
+	a=0;
+	for(int i=0; i<30; ++i)
+	{
+		if(((n>>i)&1) == 1)
+				a=i;
+		}
+        cout<<(1<<a)-1;
+		nl
+}
+
+int main()
+{   goBabygo
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+}
---
+++ A_Elections.cpp
@@ -0,0 +1,59 @@
+//Aurthor:- Azrul Amaline
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl;
+#define YES cout<<"YES";
+#define NO cout<<"NO";
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define goBabygo {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int solve(){
+    ll n,m,i,j,k,x,y,z,a,b,c,cnt=0,tmp=0,sum=0;
+    cin>>a>>b>>c;
+    if(a==b&&b==c){
+        cout<<"1 1 1";
+        nl
+        return 0;
+    }
+    else {
+        x=max(a,max(b,c))+1;
+        a=x-a;
+        b=x-b;
+        c=x-c;
+        if(a==1)
+        a=0;
+        if(b==1)
+        b=0;
+        if(c==1)
+        c=0;
+    }
+
+    cout<<a<<" "<<b<<" "<<c;
+    nl
+    return 0;
+    
+}
+
+int main()
+{   goBabygo
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+}
