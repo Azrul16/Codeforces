e24a062f5ce6f7bd70aeab55ffdf8f361458ea2a
Azrul Amaline <azrul.amaline16@gmail.com>
2022-12-20 00:50:57 +0600

    20/Dec/2022

---
+++ A_QAQ.cpp
@@ -0,0 +1,48 @@
+//Aurthor:- Azrul Amaline
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl;
+#define YES cout<<"YES";
+#define NO cout<<"NO";
+#define vll(v) vector<ll> (v);
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define goBabygo {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int solve(){
+    ll n,m,i,j,k,x,y,z,a,b,c,cnt=0,tmp=0,sum=0;
+    string s;
+    cin>>s;
+    fl(i,0,s.size()){
+        fl(j,i,s.size()){
+            fl(k,j,s.size()){
+                if(s[i]=='Q'&&s[j]=='A'&&s[k]=='Q')
+                sum++;
+            }
+        }
+    }
+    cout<<sum;
+}
+
+int main()
+{   goBabygo
+    ll t=1;
+    //cin>>t;
+    while(t--)
+    solve();
+}
