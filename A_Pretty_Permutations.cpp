a69365f8636436d307ccfec708b0f626882995ba
Azrul Amaline <azrul.amaline16@gmail.com>
2022-12-25 08:31:37 +0600

    25Dec2022

---
+++ A_Pretty_Permutations.cpp
@@ -0,0 +1,50 @@
+//Aurthor:- Azrul Amaline
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl;
+#define YES cout<<"YES";
+#define NO cout<<"NO";
+#define vll(v) vector<ll> (v);
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define goBabygo {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int solve(){
+    ll n,m,i,j,k,x,y,z,a,b,c,cnt=0,tmp=0,sum=0;
+    cin>>n;
+    cout<<2;
+    if(n>3&&n){
+        fl(i,1,n){
+        if(i==2)
+        cout<<' '<<4;
+        else cout<<' '<<i;
+        }
+    }
+    else 
+
+    nl
+    
+}
+
+int main()
+{   goBabygo
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+}
