0d4fbfb63b89307a729a04ebf5ec051ccacf64fa
Azrul Amaline <azrul.amaline16@gmail.com>
2023-01-27 21:49:48 +0600

    27Jan2023

---
+++ B_Taisia_and_Dice.cpp
@@ -0,0 +1,57 @@
+//Aurthor:- Azrul Amaline
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl;
+#define YES cout<<"YES";
+#define NO cout<<"NO";
+#define vll(v) vector<ll> (v);
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define vin(v,n) { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define goBabygo {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int solve(){
+    ll n,m,i,j,k,x,y,z,a,b,c,cnt=0,tmp=0,ans=0,p;
+    cin>>n>>m>>k;
+    p=m-k;
+       cout<<p<<" ";
+       ans=k%(n-1);
+       cnt=k/(n-1);
+       fl(i,1,n) {
+          if(ans!=0 && cnt<6) {
+              if(p-cnt<=ans) {
+                cout<<p<<" ";
+                ans-=p-cnt;
+              }
+              else {
+                cout<<cnt+ans<<" ";
+                ans=0;
+              }
+          }
+          else cout<<cnt<<" ";
+       }
+       nl;
+}
+
+int main()
+{   goBabygo
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+}
