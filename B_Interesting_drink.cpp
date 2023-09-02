87925884196fdda44f048c8f5311ac814361ad2d
Azrul Amaline <65326631+Azrul16@users.noreply.github.com>
2023-09-02 23:49:28 +0600

    2Sep2023

--- B_Interesting_drink.cpp
+++ B_Interesting_drink.cpp
@@ -1,5 +1,3 @@
-
-
 //Aurthor:- Azrul Amaline
 #include<bits/stdc++.h>
 using namespace std;
@@ -14,9 +12,9 @@ typedef long long ll;
 #define YES cout<<"YES";
 #define NO cout<<"NO";
 #define vll(v) vector<ll> (v);
-#define vin(v,n) { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
 #define vsort(v) sort(v.begin(), v.end());
 #define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define vin(v,n) { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
 #define all(x) x.begin(),x.end()
 #define pb push_back
 #define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
@@ -27,23 +25,26 @@ typedef long long ll;
 #define goBabygo {ios::sync_with_stdio(false); cin.tie(NULL);}
 //--------------------------------------------------------------
 
+//Global Declaration 
+//--------------------------------------------------------------
+const int N=1e5+10;
+int dp[N];
+//--------------------------------------------------------------
+
+
+int solve(){
+    ll n,m,i,j,k,x,y,z,a,b,c,cnt=0,tmp=0,sum=0;
+    string s,s1,sp;
+    vector<ll>v;
+    vector<string>vs;
+    
+    
+}
+
 int main()
 {   goBabygo
-    ll n,m,i,j,k,x,y,z,a,b,c,cnt=0,tmp=0,sum=0;
-    cin>>n;
-    ll arr[n];
-    fl(i,0,n)cin>>arr[i];
-    sort(arr,arr+n);
-    cin>>a;
-    while(a--){
-        cin>>x;
-        fl(i,0,n){
-            if(x>=arr[i])cnt++;
-            else break;
-        }
-        cout<<cnt<<endl;
-        cnt=0;
-    }
-
-    
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
 }
